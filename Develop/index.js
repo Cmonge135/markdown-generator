2a7ce550df52330c83c68a48ad98a34612e518c5
Carlos <cmonge135@gmail.com>
2021-04-29 02:29:29 -0400

    added content to given files

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,78 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const generateMarkdown = require('./utils/generateMarkdown.js')
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'GithubUsername',
+        message: 'What is your GitHub username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      },
+      {
+        type: 'input',
+        name: 'projectName',
+        message: "What is your project's name?",
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project.',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "ISC", "Apache", "none"]
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'What command should be run to install dependencies?',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?',
+      },
+      {
+        type: 'input',
+        name: 'contributions',
+        message: 'What does the user need to know about contributing to the repo?',
+      }, 
+];
+const promptUser = () => {
+    return inquirer.prompt(questions)
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (data) => {
+    fs.writeFile('README.md', data, (error) =>
+    error ? console.log('Error!') : console.log('Success!'));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    promptUser()
+      .then((data) => writeFileAsync('README.md', generateMarkdown(data)))
+      .then(() => console.log('Successfully wrote to README.md!!!'))
+      .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,92 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = (license) => {
+  let licenseType;
+
+  switch (license) {
+    case 'MIT':
+      licenseType = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case 'ISC':
+      licenseType = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+      break;
+    case 'Apache License 2.0':
+      licenseType = '[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      break;
+    case 'none':
+      licenseType = '';
+      break;
+  }
+  return licenseType;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = (license) => {
+  let licenseLink;
+
+  switch (license) {
+    case 'MIT':
+      licenseLink = 'https://opensource.org/licenses/MIT';
+      break;
+    case 'ISC':
+      licenseLink = 'https://opensource.org/licenses/ISC';
+      break;
+    case 'Apache':
+      licenseLink = 'https://opensource.org/licenses/Apache-2.0';
+      break;
+    case 'none':
+      licenseLink = '';
+      break;
+    
+  }
+  return licenseLink;
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateMarkdown = (data) => {
+  
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  return `
+  # Project Name:
+  ${data.projectName}
 
+  ## License
+  ${licenseBadge} | [View License](${licenseLink})
+
+  ## Table of Contents
+- [Title](#Project-Name)
+- [License](#License)
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contributions](#Contributions)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+  ## Description
+  ${data.description}
+
+  ## Installation 
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributions
+  ${data.contributions}
+
+  ## Tests
+  ${data.test}
+
+  ## Questions
+  For any questions or suggestions please contact at ${data.email} or visit [https://github.com/${data.GithubUsername}](https://github.com/${data.GithubUsername})
 `;
 }
 
